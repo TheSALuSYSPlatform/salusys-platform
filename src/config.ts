b9bf8f2e825585c4cb30f1831fd2cb293a81499c
stephencpowell-hue <stephencpowell@gmail.com>
2026-02-02 07:53:18 -0600

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
+  website: "https://YOUR-USERNAME.github.io/salusys-platform/", // replace this with your deployed domain
+  author: "SALuSYS",
   profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  desc: "Systems for Safer Places",
+  title: "The SALuSYS Platform",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
